237b25699e26cb348d68c77b169fc5fbb35589f0
liamcannon <liam.cannon@mymail.champlain.edu>
2022-02-24 17:30:49 -0500

    triangle

--- Sources/SVGLibrary/Point.swift
+++ Sources/SVGLibrary/Point.swift
@@ -7,7 +7,11 @@
 
 public typealias Point = (x: Int, y: Int)
 
+
 // The midpoint between two Points.
 public func midpoint(_ p1: Point, _ p2: Point) -> Point {
-    // YOUR CODE HERE
+    let mid: Point
+    mid.x = (p1.x + p2.x) / 2
+    mid.y = (p1.y + p2.y) / 2
+    return mid
 }
--- Sources/SierpinskiSwift/main.swift
+++ Sources/SierpinskiSwift/main.swift
@@ -7,11 +7,13 @@
 import SVGLibrary
 
 // YOUR CODE HERE
-
 // Draw a triangle between the three points and then recursively draw
 // three more triangles in each of the three corners of the first triangle.
 func sierpinski(p1: Point, p2: Point, p3: Point, level: Int, figure: SVG) {
-    // YOUR CODE HERE
+    if (level <= 0) {return }
+    sierpinski(p1: p1, p2: midpoint(p1, p2), p3: midpoint(p1, p3), level: level - 1, figure: figure)
+    sierpinski(p1: p2, p2: midpoint(p1, p2), p3: midpoint(p2, p3), level: level - 1, figure: figure)
+    sierpinski(p1: p3, p2: midpoint(p1, p3), p3: midpoint(p2, p3), level: level - 1, figure: figure)
 }
 
 // Start the algorithm off using a 300x300 canvas and its largest triangle
@@ -21,7 +23,9 @@ func main() {
     let p1: Point = (0, 300)
     let p2: Point = (150, 0)
     let p3: Point = (300, 300)
+    figure.start_graphics(width: 300, height: 300)
     sierpinski(p1: p1, p2: p2, p3: p3, level: 5, figure: figure)
+    figure.end_graphic()
     figure.write(filePath: "sierpinski.svg")
 }
 
